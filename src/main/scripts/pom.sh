d210e24f66112356342f839eaecb35149bd16aa1
Jacky Chan <libing.chen@gmail.com>
2013-02-21 16:22:32 +0800

    minor bug fix

--- src/main/scripts/pom.sh
+++ src/main/scripts/pom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 app_dir=`dirname $0`
-java -jar ${app_dir}/jar_pom_info-0.0.1.jar:${app_dir}/jdom-2.0.2.jar $1
+java -cp ${app_dir}/lib/jar_pom_info-0.0.1.jar:${app_dir}/lib/jdom-2.0.2.jar org.mvnsearch.tools.JarPomViewer $1
